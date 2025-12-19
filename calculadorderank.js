21e38b4f4e343abaa276bba0b30f44e88d4be93a
Geovana Ferlin <gferlin36@gmail.com>
2025-12-19 14:21:56 -0300

    Calculadora de Rank

---
+++ calculadorderank.js
@@ -0,0 +1,31 @@
+function calcularRank(vitorias, derrotas) {
+    let saldoVitorias = vitorias - derrotas;
+    let nivel = "";
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return { saldoVitorias, nivel };
+}
+
+let vitorias = 75;
+let derrotas = 20;
+
+let resultado = calcularRank(vitorias, derrotas);
+
+console.log(
+    `O Herói tem de saldo de ${resultado.saldoVitorias} está no nível de ${resultado.nivel}`
+);
